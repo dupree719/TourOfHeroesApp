264374ec1b593c411916ad68e4abe0acf55749b7
dupree719 <isaiahdup7@gmail.com>
2022-12-06 10:51:59 -0500

    Mostly finished

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -1,9 +1,13 @@
 import { TestBed } from '@angular/core/testing';
+import { RouterTestingModule } from '@angular/router/testing';
 import { AppComponent } from './app.component';
 
 describe('AppComponent', () => {
   beforeEach(async () => {
     await TestBed.configureTestingModule({
+      imports: [
+        RouterTestingModule
+      ],
       declarations: [
         AppComponent
       ],
@@ -16,16 +20,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'myveryfirstapp'`, () => {
+  it(`should have as title 'myfirstapp'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('myveryfirstapp');
+    expect(app.title).toEqual('myfirstapp');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('.content span')?.textContent).toContain('myveryfirstapp app is running!');
+    expect(compiled.querySelector('.content span')?.textContent).toContain('myfirstapp app is running!');
   });
 });
